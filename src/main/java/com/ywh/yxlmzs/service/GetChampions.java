697d2097f8b5b767ce0031e30398b61a62dce85e
20186 <2018673788@qq.com>
2024-07-11 15:10:43 +0800

    Revert "修改"
    This reverts commit 688990c7

--- src/main/java/com/ywh/yxlmzs/service/GetChampions.java
+++ src/main/java/com/ywh/yxlmzs/service/GetChampions.java
@@ -1,41 +1,29 @@
 package com.ywh.yxlmzs.service;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.annotation.Resource;
 import kong.unirest.JsonResponse;
 import kong.unirest.Unirest;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
-
 @Service
 public class GetChampions {
 
-    @Resource
-    ObjectMapper objectMapper;
-
-    public void getChampions() throws IOException {
-
-        ClassPathResource resource = new ClassPathResource("static/versions.json");
-        Path tempFile = Files.createTempFile("versions", ".json");
-        Files.copy(resource.getInputStream(), tempFile, StandardCopyOption.REPLACE_EXISTING);
-
-        File file = new File(tempFile.toString());
-        JsonNode versionNode = objectMapper.readTree(file);
-        String version = versionNode.get("version").asText();
-
-        ClassPathResource resource1 = new ClassPathResource("static/champion.json");
-        Path tempFile1 = Files.createTempFile("champion", ".json");
-        Files.copy(resource1.getInputStream(), tempFile, StandardCopyOption.REPLACE_EXISTING);
-
+    public void getChampions() {
+        String versionFilePath = System.getProperty("user.dir") + "/version.txt";
+        StringBuilder versionContent = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(versionFilePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                versionContent.append(line).append("\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String version = versionContent.toString().trim();
+        String projectRoot = System.getProperty("user.dir");
         JsonResponse response = (JsonResponse) Unirest.get("http://ddragon.leagueoflegends.com/cdn/"+version+"/data/zh_CN/champion.json")
                     .asJson();
-            String championPath = tempFile1.toString();
+            String championPath = projectRoot + "/src/main/resources/static/champion.json";
             try (FileWriter writer = new FileWriter(championPath)) {
                 writer.write(response.getBody().toString());
             } catch (IOException e) {
